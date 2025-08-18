aa9c06dc3e54126995dcbbad380bc3bd89eaf057
yangweijian <yangweijian@stargraph.cn>
2025-08-18 23:33:18 +0800

    cf_sst_check: add multithreads support

--- tools/cf_repairer.cc
+++ tools/cf_repairer.cc
@@ -4,15 +4,18 @@
 // cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DROCKSDB_BUILD_SHARED=OFF
 // -DBUILD_SHARED_LIBS=OFF -DWITH_GFLAGS=1 make cf_repairer -j 16
 
+#include <cstddef>
 #include <cstdint>
 #include <cstdio>
 #include <memory>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 #include "file/filename.h"
+#include "port/port_posix.h"
 #include "rocksdb/advanced_options.h"
 #include "rocksdb/convenience.h"
 #include "rocksdb/db.h"
@@ -23,6 +26,7 @@
 #include "rocksdb/utilities/options_util.h"
 #include "rocksdb/version.h"
 #include "table/sst_file_dumper.h"
+#include "util/channel.h"
 #include "util/gflags_compat.h"
 #include "util/stderr_logger.h"
 #include "util/string_util.h"
@@ -35,7 +39,7 @@ DEFINE_string(
     cf_names, "",
     "The names of cf to operate with, multiple cf shoule be comma-separated");
 DEFINE_string(command, "", "The command to run");
-DEFINE_int32(workers, 1, "The number of worker to run repair process");
+DEFINE_uint32(workers, 4, "The number of worker to run repair process");
 
 class CfRepairer {
  public:
@@ -47,6 +51,11 @@ class CfRepairer {
   void OpenDB(bool read_only);
   void CloseDB();
   void RunSstCheck();
+
+  void RunSstCheckThreads(std::string, std::vector<std::vector<std::string>>,
+                          rocksdb::channel<std::string>*);
+  void JoinSstCheckThreads(rocksdb::channel<std::string>*);
+  void ReceiveCheckResults(std::string, rocksdb::channel<std::string>*);
   rocksdb::Status CheckSst(const std::string&);
 
   rocksdb::DB* db_;
@@ -61,7 +70,12 @@ class CfRepairer {
   std::string cp_path_suffix_;
 
   std::shared_ptr<rocksdb::Logger> logger_;
+
+  std::vector<std::unique_ptr<rocksdb::port::Thread>> threads_;
+  // std::vector<std::vector<std::string>> chunked_sst_lists_;
+  std::unordered_map<std::string, std::vector<std::string>> corruption_ssts_;
 };
+
 const char* USAGE =
     "USAGE: \n"
     "  cf_repairer -db_path <DBPATH> -cf_name <CFNAME> -command <COMMAND> "
@@ -69,6 +83,29 @@ const char* USAGE =
 const char* STAGE_0 = "LoadOptions";
 const char* STAGE_1 = "OpenDB";
 const char* STAGE_2 = "CheckSst";
+
+void ChunkSstFiles(const std::vector<std::string>& files,
+                   std::vector<std::vector<std::string>>* results) {
+  size_t min_chunk_size = 3;
+  size_t chunk_size = files.size() / FLAGS_workers + 1;
+  chunk_size = std::max(min_chunk_size, chunk_size);
+  if (files.empty()) {
+    fprintf(stdout, "[%s] sst-files empty! \n", STAGE_2);
+    exit(-1);
+  }
+
+  fprintf(stdout, "[%s] sst-files count: %ld, workers: %d, chunk-size: %ld \n",
+          STAGE_2, files.size(), FLAGS_workers, chunk_size);
+  auto it = files.cbegin();
+
+  while (it != files.cend()) {
+    size_t step_size =
+        size_t(files.cend() - it) > chunk_size ? chunk_size : files.cend() - it;
+    auto it_part_end = it + step_size;
+    results->emplace_back(it, it_part_end);
+    it = it_part_end;
+  }
+}
 
 CfRepairer::CfRepairer()
     : db_(nullptr), db_path_(FLAGS_db_path), cp_path_suffix_("_checkpoint") {
@@ -111,6 +148,7 @@ CfRepairer::CfRepairer()
             "[%s] not found column families from options, try to load from "
             "manifests\n",
             STAGE_0);
+
     // try to find cf by manifest
     std::vector<std::string> cf_list;
     s = rocksdb::DB::ListColumnFamilies(options_, db_path_, &cf_list);
@@ -225,11 +263,76 @@ rocksdb::Status CfRepairer::CheckSst(const std::string& file_path) {
   return s;
 }
 
+void CfRepairer::RunSstCheckThreads(
+    std::string cf_name,
+    std::vector<std::vector<std::string>> chunked_sst_lists,
+    rocksdb::channel<std::string>* ck_chan) {
+  size_t thread = 0;
+  for (auto chunk_ssts : chunked_sst_lists) {
+    auto t = new rocksdb::port::Thread(
+        [this, ck_chan](std::vector<std::string> sst_files, std::string cf,
+                        size_t thread_id) {
+          size_t total_files = sst_files.size();
+          size_t bad_files = 0;
+          for (auto file : sst_files) {
+            std::string file_path(std::string(this->db_path_) + file);
+            rocksdb::Status s = this->CheckSst(file_path);
+            if (!s.ok()) {
+              REPAIRER_LOG(this->logger_,
+                           "[%s] [cf = %s] [thread = %ld] sst %s is corrupted, "
+                           "detail: %s \n",
+                           STAGE_2, cf.c_str(), thread_id, file.c_str(),
+                           s.ToString().c_str());
+
+              ck_chan->write(std::move(file));
+              bad_files++;
+            }
+          }
+          REPAIRER_LOG(this->logger_,
+                       "[%s] [cf = %s] [thread = %ld] worker finished, "
+                       "summary: %ld files checked, %ld files corrupted",
+                       STAGE_2, cf.c_str(), thread_id, total_files, bad_files);
+        },
+        chunk_ssts, cf_name, thread);
+    threads_.emplace_back(t);
+    thread++;
+  }
+}
+
+void CfRepairer::JoinSstCheckThreads(rocksdb::channel<std::string>* ck_chan) {
+  for (auto t = threads_.begin(); t != threads_.end(); ++t) {
+    if (t->get() != nullptr) {
+      t->get()->join();
+      t->reset();
+    }
+  }
+  threads_.clear();
+  ck_chan->sendEof();
+}
+
+void CfRepairer::ReceiveCheckResults(std::string cf_name,
+                                     rocksdb::channel<std::string>* ck_chan) {
+  auto& op = corruption_ssts_[cf_name];
+  while (true) {
+    std::string file;
+    bool s = ck_chan->read(file);
+    if (!s) {
+      break;
+    }
+    op.emplace_back(std::move(file));
+  }
+  fprintf(stdout,
+          "[%s] total corruption ssts: %ld, has checked column families: "
+          "%ld \n",
+          STAGE_2, corruption_ssts_[cf_name].size(), corruption_ssts_.size());
+}
+
 void CfRepairer::RunSstCheck() {
   std::unordered_map<std::string, rocksdb::ColumnFamilyHandle*> cf_handles_map;
   for (auto cfh : cf_handles_) {
     cf_handles_map.emplace(std::string(cfh->GetName()), cfh);
   }
+
   for (const auto& cf : target_cf_names_) {
     fprintf(stdout, "[%s] [cf = %s] start to check sst files ... \n", STAGE_2,
             cf.c_str());
@@ -261,22 +364,57 @@ void CfRepairer::RunSstCheck() {
         }
       }
     }
-    fprintf(stdout, "[%s] [cf = %s] found %ld sst files \n", STAGE_2,
-            cf.c_str(), cf_sst_files.size());
+    fprintf(stdout, "[%s] [cf = %s] has found %ld sst files to check.\n",
+            STAGE_2, cf.c_str(), cf_sst_files.size());
+
+    // chunk sst
+    std::vector<std::vector<std::string>> chunked_sst_lists;
+    ChunkSstFiles(cf_sst_files, &chunked_sst_lists);
+
+    int ck = 0;
+    size_t chunk_total = 0;
+    fprintf(stdout, "[%s] [cf = %s] has %ld chunks, workers: %d. \n", STAGE_2,
+            cf.c_str(), chunked_sst_lists.size(), FLAGS_workers);
+
+    for (const auto& item : chunked_sst_lists) {
+      fprintf(stdout, "[%s] [cf = %s] chunk %d size: %ld \n", STAGE_2,
+              cf.c_str(), ck, item.size());
+      chunk_total += item.size();
+      ck++;
+    }
+
+    if (chunked_sst_lists.size() > FLAGS_workers) {
+      fprintf(stdout,
+              "[%s] [cf = %s] [BUG] Bad chunk results: chunks more than "
+              "workers. \n",
+              STAGE_2, cf.c_str());
+      exit(-1);
+    }
+
+    if (chunk_total != cf_sst_files.size()) {
+      fprintf(stdout,
+              "[%s] [cf = %s] [BUG] Bad chunk results: chunked sst lists not "
+              "equal to original. \n",
+              STAGE_2, cf.c_str());
+      exit(-1);
+    }
 
     // check sst
-    for (const auto& f : cf_sst_files) {
-      std::string sst_path = std::string(db_path_) + f;
-      rocksdb::Status s = CheckSst(sst_path);
-      if (s.ok()) {
-        REPAIRER_LOG(logger_, "[%s] [cf = %s] sst: %s check ok.", STAGE_2,
-                     cf.c_str(), sst_path.c_str());
-      } else {
-        REPAIRER_LOG(logger_, "[%s] [cf = %s] sst: %s check failed: %s.",
-                     STAGE_2, cf.c_str(), sst_path.c_str(),
-                     s.ToString().c_str());
-      }
-    }
+    fprintf(stdout, "[%s] [cf = %s] + start sst check workers ... \n", STAGE_2,
+            cf.c_str());
+    rocksdb::channel<std::string> ck_chan;
+    RunSstCheckThreads(cf, std::move(chunked_sst_lists), &ck_chan);
+    fprintf(stdout,
+            "[%s] [cf = %s] + wait sst check workers to "
+            "finish ... \n",
+            STAGE_2, cf.c_str());
+    JoinSstCheckThreads(&ck_chan);
+    fprintf(stdout, "[%s] [cf = %s] + all sst check workers finished.\n",
+            STAGE_2, cf.c_str());
+    ReceiveCheckResults(cf, &ck_chan);
+    fprintf(stdout,
+            "[%s] [cf = %s] + all sst check workers results kept finished.\n",
+            STAGE_2, cf.c_str());
   }
 }
 
